85446e7be0acfd6ad1291e2b3a6ee1c645e698ad
Aryan raj <aryanbanthu@gmail.com>
2026-03-31 19:48:27 +0530

    Added Main class  logic to test randomness and concurrency.

--- Main.java
+++ Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+
+        TaskQueue queue = new TaskQueue();
+
+        // Create workers
+        Worker w1 = new Worker(queue, "Worker-1");
+        Worker w2 = new Worker(queue, "Worker-2");
+        Worker w3 = new Worker(queue, "Worker-3");
+
+        // Start threads
+        w1.start();
+        w2.start();
+        w3.start();
+
+        // Add tasks
+        for (int i = 1; i <= 10; i++) {
+            queue.addTask(new Task(i));
+        }
+    }
 }
