7c1e7ad1a4422c4d5ab197c1070f637258caf6d3
Aryan raj <aryanbanthu@gmail.com>
2026-03-31 19:43:45 +0530

    Added Worker class constructor .

--- Worker.java
+++ Worker.java
@@ -1,3 +1,9 @@
 public class Worker extends Thread{
     private TaskQueue taskQueue;
+
+    public Worker(TaskQueue taskQueue, String name) {
+        super(name);
+        this.taskQueue = taskQueue;
+    }
+
 }
