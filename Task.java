131d08fbcf3304e9bbc3a942490c7dec94d33cbd
Aryan raj <aryanbanthu@gmail.com>
2026-03-31 19:37:14 +0530

    Added overriden Task class run method , simulating a work with sleep method.

--- Task.java
+++ Task.java
@@ -7,6 +7,12 @@ public class Task implements Runnable{
 
     @Override
     public void run() {
-
+        System.out.println("Task " + taskId + " started by " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(1000); // simulate work
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Task " + taskId + " completed by " + Thread.currentThread().getName());
     }
 }
