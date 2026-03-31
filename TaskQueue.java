ba0090f199a288f568699509355bcf3192583cb7
Aryan raj <aryanbanthu@gmail.com>
2026-03-31 19:40:46 +0530

    Added method in TaskQueue class for adding a task in the queue. .

--- TaskQueue.java
+++ TaskQueue.java
@@ -3,4 +3,12 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class TaskQueue{
     private BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>();
+
+    public void addTask(Runnable task) {
+        try {
+            queue.put(task);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
