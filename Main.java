6f071470260f3b2cc53861bfa3076116d6f48044
Aryan raj <aryanbanthu@gmail.com>
2026-04-01 17:20:35 +0530

    Main method tests.

--- Main.java
+++ Main.java
@@ -1,21 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         TaskQueue queue = new TaskQueue();
 
-        // Create workers
         Worker w1 = new Worker(queue, "Worker-1");
         Worker w2 = new Worker(queue, "Worker-2");
-        Worker w3 = new Worker(queue, "Worker-3");
 
-        // Start threads
         w1.start();
         w2.start();
-        w3.start();
 
-        // Add tasks
         for (int i = 1; i <= 10; i++) {
             queue.addTask(new Task(i));
         }
+
+        Thread.sleep(5000); // let tasks run
+
+        // Shutdown workers
+        w1.shutdown();
+        w2.shutdown();
     }
 }
