38f3468f1ad1a7ec1ef42f381fd461a4ed5a2e59
Aryan raj <aryanbanthu@gmail.com>
2026-04-02 20:24:59 +0530

    Added members of TaskScheduler.

--- scheduler/TaskScheduler.java
+++ scheduler/TaskScheduler.java
@@ -1,4 +1,9 @@
 package scheduler;
 
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class TaskScheduler {
+    private ThreadPoolExecutor executor;
+    private ScheduledExecutorService scheduler;
 }
