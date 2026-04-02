8be9340effda6264dbd6d119d3f1cf747edf0980
Aryan raj <aryanbanthu@gmail.com>
2026-04-02 20:31:31 +0530

    Added constructor for TaskScheduler.

--- scheduler/TaskScheduler.java
+++ scheduler/TaskScheduler.java
@@ -1,9 +1,26 @@
 package scheduler;
 
+import task.Task;
+
+import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadPoolExecutor;
 
 public class TaskScheduler {
     private ThreadPoolExecutor executor;
     private ScheduledExecutorService scheduler;
+
+    public TaskScheduler(int poolSize) {
+        executor = new ThreadPoolExecutor(
+                poolSize,
+                poolSize,
+                0L,
+                TimeUnit.MILLISECONDS,
+                new PriorityBlockingQueue<>()
+        );
+
+        scheduler = Executors.newScheduledThreadPool(2);
+    }
 }
+
+
