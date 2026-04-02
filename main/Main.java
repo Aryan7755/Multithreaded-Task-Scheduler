0dc3fae6955da02dcc9de46258501dfe3e80f755
Aryan raj <aryanbanthu@gmail.com>
2026-04-02 20:37:12 +0530

    Added priority tasks in main.

--- main/Main.java
+++ main/Main.java
@@ -6,5 +6,11 @@ import task.Task;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         TaskScheduler scheduler = new TaskScheduler(3);
+
+        // Submit priority tasks
+        scheduler.submitTask(new Task(1, 2));
+        scheduler.submitTask(new Task(2, 5));
+        scheduler.submitTask(new Task(3, 1));
+        scheduler.submitTask(new Task(4, 4));
     }
 }
