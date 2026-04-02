0fabdb6e25fe713c04476a48077ec55e976d1410
Aryan raj <aryanbanthu@gmail.com>
2026-04-02 20:36:26 +0530

    Added 3 scheduler objects in main.

--- main/Main.java
+++ main/Main.java
@@ -1,26 +1,10 @@
-//package main;
-//
-//import task.Task;
-//
-//public class Main {
-//    public static void main(String[] args) throws InterruptedException {
-//
-////        TaskQueue queue = new TaskQueue();
-//
-//        Worker w1 = new Worker(queue, "Worker-1");
-//        Worker w2 = new Worker(queue, "Worker-2");
-//
-//        w1.start();
-//        w2.start();
-//
-//        for (int i = 1; i <= 10; i++) {
-//            queue.addTask(new Task(i));
-//        }
-//
-//        Thread.sleep(5000); // let tasks run
-//
-//        // Shutdown workers
-//        w1.shutdown();
-//        w2.shutdown();
-//    }
-//}
+package main;
+
+import scheduler.TaskScheduler;
+import task.Task;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TaskScheduler scheduler = new TaskScheduler(3);
+    }
+}
